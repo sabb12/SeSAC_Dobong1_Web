b5676c5b557b2d4fed717250320e8007a0242674
sabb12 <sabb0923@gmail.com>
2024-03-29 09:51:59 +0900

    25-react-router-practice

--- 25-react-router-practice/src/App.js
+++ 25-react-router-practice/src/App.js
@@ -1,34 +1,18 @@
 import { Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
-import StudentPage from "./pages/StudentPage";
-import MainPage from "./pages/MainPage";
+import Student from "./pages/Student";
+import PracticeHeader from "./components/PracticeHeader";
+import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Profile from "./pages/Query";
-import CodingOn from "./pages/CodingOn";
-import Query from "./pages/Query";
-import "./styles/style.scss";
-// import { useEffect, useState } from "react";
-// import axios from "axios";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
-
-const MyLink = styled(Link)`
-  text-decoration: none;
-  color: black;
-`;
+import "./styles/common.scss";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      {/* <MyLink>메인페이지 입니다</MyLink> */}
+      <PracticeHeader />
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/studentPage" element={<StudentPage />} />
-        <Route path="/codingOn" element={<CodingOn />} />
-        <Route path="/query" element={<Query />} />
-
+        실습
+        <Route path="/" element={<Index />}></Route>
+        <Route path="/student/:name" element={<Student />}></Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
--- 25-react-router-practice/src/pages/NotFound.jsx
+++ 25-react-router-practice/src/pages/NotFound.jsx
@@ -1,10 +1,7 @@
-import { Link } from "react-router-dom";
-
 export default function NotFound() {
   return (
     <>
-      <h1>page not found</h1>
-      <Link to="/">홈으로 이동하기</Link>
+      <h1>404 page not found</h1>
     </>
   );
 }
