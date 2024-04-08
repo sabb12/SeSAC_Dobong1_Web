d3f3a5a44dfe9eb799a535f7479f04cdfa1318b8
sabb12 <sabb0923@gmail.com>
2024-04-08 16:29:23 +0900

    s3 image upload with multer-s3&aws-sdk

---
+++ 31-aws-s3/app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const multer = require("multer");
+const aws = require("aws-sdk");
+const multerS3 = require("multer-s3");
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT;
+
+// 미들웨어 설정
+app.set("view engine", "ejs");
+
+// aws 설정
+aws.config.update({
+  accessKeyId: process.env.AWS_S3_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
+  region: process.env.AWS_S3_REGION,
+});
+
+const s3 = new aws.S3();
+
+// s3관련 멀터 설정
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.AWS_S3_BUCKET,
+    acl: "public-read", // 파일 접근 권한
+    key: (req, file, cb) => {
+      cb(null, Date.now().toString() + "-" + file.originalname);
+    },
+  }),
+});
+
+// GET / index.ejs 렌더링
+app.get("/", (req, res) => {
+  res.render("index", { imageUrl: "" });
+});
+
+// POST /upload
+app.post("/upload", upload.single("image"), (req, res) => {
+  console.log(req.file);
+  // res.send("이미지 업로드 중...");
+  res.render("index", { imageUrl: req.file.location });
+});
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
